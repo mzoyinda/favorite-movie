5734a2280b50bfa3bb9bf0a7bc50d3196f957e66
oyindamola <oyindamoladawodu22@gmail.com>
2023-10-27 12:58:21 +0100

    latest update

--- Frontend/src/components/Form.jsx
+++ Frontend/src/components/Form.jsx
@@ -48,7 +48,8 @@ const Form = ({ getMovies }) => {
 
   const getSingleMovie = async (movieId) => {
     // get movie details with movie id
-    const result = await fetch(`http://www.omdbapi.com/?i=${movieId}&apikey=${process.env.REACT_APP_API_KEY}`);
+    const URL = `http://www.omdbapi.com/?i=${movieId}&apikey=${process.env.REACT_APP_API_KEY}`
+    const result = await fetch(`${URL}`);
     const data = await result.json();
     console.log(data);
     if (data.Response === "True")
